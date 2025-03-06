ac162ab7b8bbde6fd8083a1f71585f9afb38d5bd
aliparsa <aliparsa9@gmail.com>
2025-03-06 08:15:00 +0430

    Login, register and auth done

--- resources/views/Frontend/layout/header.blade.php
+++ resources/views/Frontend/layout/header.blade.php
@@ -14,6 +14,30 @@
 	          <li class="nav-item"><a href="car.html" class="nav-link">Cars</a></li>
 	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
 	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+			  @if (Route::has('login'))
+			 	 @auth
+				  <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li
+				@else
+				
+	          <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+	          <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
+				@endauth
+			@endif
+
 	        </ul>
 	      </div>
 	    </div>
